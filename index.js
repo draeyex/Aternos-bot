f8e807f9ad324a44f711b799c83bb356cf0da768
draeyex <draeyex@gmail.com>
2026-03-09 12:43:27 +0600

    Update Minecraft server host in createBot function

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const BOT_PASSWORD = "boqad112022";
 function createBot() {
   console.log("Attempting to connect to Minecraft server...");
   const bot = mineflayer.createBot({
-    host: "forcesmp.qzz.io",
+    host: "force_smp.aternos.me",
     version: false,
     username: "lvnger",
     port: 20039,
